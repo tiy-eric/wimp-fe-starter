786cee43c6e13317b9faf2801e69b1c2b78ee3b3
Eric Schwartz <eric.schwartz@theironyard.com>
2018-01-02 17:10:57 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  movies;
+
+  actors;
+ 
+  constructor(private dataservice: DataService){}
+
+  getMovies(){
+
+    this.dataservice.getRecords("movies")
+      .subscribe(moviesFromAPI => {
+        this.movies = moviesFromAPI;
+      })
+
+  }
+
+  getActors(){
+    
+        this.dataservice.getRecords("actors")
+          .subscribe(actorsFromAPI => {
+            this.actors = actorsFromAPI;
+          })
+    
+      }
+
+  ngOnInit(){
+    this.getMovies();
+    this.getActors();
+  }
+
 }
